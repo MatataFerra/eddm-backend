8ef718c98fa2e5b1efdef2848f6e782fceb25a8f
matataferra <matias.agf@gmail.com>
2025-02-26 21:39:15 +1030

    Elimina archivos de configuración de producción y agrega soporte para despliegue en Vercel en plugins.

--- config/plugins.ts
+++ config/plugins.ts
@@ -14,4 +14,14 @@ export default ({ env }) => ({
       },
     },
   },
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_HOOK"),
+      apiToken: env("VERCEL_API_TOKEN"),
+      appFilter: env("VERCEL_NAME_APP"),
+      teamFilter: env("VERCEL_TEAM_ID"),
+      roles: ["strapi-super-admin"],
+    },
+  },
 });
